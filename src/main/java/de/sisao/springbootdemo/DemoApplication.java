1812fe92c0dba9b9986ce8b348d1f9e7ac21375d
slorenz <lorenz.stefan@gmail.com>
2018-03-22 13:55:17 +0100

    add sample response

--- src/main/java/de/sisao/springbootdemo/DemoApplication.java
+++ src/main/java/de/sisao/springbootdemo/DemoApplication.java
@@ -1,10 +1,21 @@
 package de.sisao.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
 public class DemoApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello from Jenkins-X pipeline build!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
